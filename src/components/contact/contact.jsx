6b35a8a7997451c8446966d0c3e34b61eb5c9f1c
magdiperper12 <162820515+magdiperper12@users.noreply.github.com>
2025-02-02 16:45:45 +0200

    adding some of design

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,7 +1,12 @@
 import './contact.css';
 
 function Contact() {
-	return <div className='main'>Contact</div>;
+	return (
+		<div className='main'>
+			Contact
+			<h1>hello</h1>
+		</div>
+	);
 }
 
 export default Contact;
