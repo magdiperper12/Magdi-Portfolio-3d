2ce20ef2dd2046dff5bf5491f7e07cb5b32f1fd4
magdiperper12 <162820515+magdiperper12@users.noreply.github.com>
2025-02-03 13:54:38 +0200

    adding type animation

--- src/components/hero/Speech.jsx
+++ src/components/hero/Speech.jsx
@@ -1,7 +1,24 @@
+import { TypeAnimation } from 'react-type-animation';
+
 function Speech() {
 	return (
 		<div className='bubblecontainer'>
-			<div className='buble'>text</div>
+			<div className='buble'>
+				<TypeAnimation
+					sequence={[
+						1000,
+						"Hi ther, my name is magdi , I'm a frontend developer",
+						1000,
+						'i can building interactive & user-friendly web app',
+						1000,
+						'How can i help you ...?',
+						1000,
+					]}
+					wrapper='span'
+					speed={40}
+					repeat={Infinity}
+				/>
+			</div>
 			<img
 				src='/man.png'
 				alt=''
