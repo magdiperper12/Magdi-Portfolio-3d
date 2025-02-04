4808db9e64201e2dc77e6895ec38e803228c5cb1
magdiperper12 <162820515+magdiperper12@users.noreply.github.com>
2025-02-04 19:21:13 +0200

    add text

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,8 +1,8 @@
 function Services() {
 	return (
 		<div className='services'>
-			<div className='sService left'></div>
-			<div className='sService rigth'></div>
+			<div className='sService left'>first section</div>
+			<div className='sService rigth'>secound section</div>
 		</div>
 	);
 }
