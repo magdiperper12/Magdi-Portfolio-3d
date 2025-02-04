9d33e1acdc145842a75f0b4a1b0609c392f3e096
magdiperper12 <162820515+magdiperper12@users.noreply.github.com>
2025-02-04 13:37:39 +0200

    addig new details

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,6 +1,27 @@
 import './services.css';
+import { Canvas } from '@react-three/fiber';
 function Services() {
-	return <div>services</div>;
+	return (
+		<div>
+			<div id='canvas-container'>
+				<Canvas>
+					<mesh>
+						<boxGeometry args={[2, 2, 2]} />
+						<meshPhongMaterial />
+					</mesh>
+					<ambientLight intensity={0.1} />
+					<directionalLight
+						position={[0, 0, 5]}
+						color='red'
+					/>
+					<orbitControls
+						ref={orbitControlsRef}
+						args={[camera, gl.domElement]}
+					/>
+				</Canvas>
+			</div>
+		</div>
+	);
 }
 
 export default Services;
